009fa7748cb9a96446aad8da2bae13e6440db568
Niestrat99 <niestrat99@gmail.com>
2018-12-16 20:51:25 +0100

    Added several new commands
    YO CHECK DIS OUT MATE

--- src/main/java/io/github/teambanhammer/tbht_main.java
+++ src/main/java/io/github/teambanhammer/tbht_main.java
@@ -2,19 +2,21 @@ package io.github.teambanhammer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class tbht_main extends JavaPlugin {
 
-
+private List<Player>tpoff = new ArrayList<>();
 
     @Override
     public void onEnable (){
@@ -23,7 +25,7 @@ public class tbht_main extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (label.equalsIgnoreCase("tphelp")){
+        if (label.equalsIgnoreCase("tphelp")) {
             sender.sendMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.UNDERLINE + "Teleport Commands");
             sender.sendMessage("/tpa <Player> - Sends the targeted player a teleport request to where they are.");
             sender.sendMessage("/tpahere <Player> - Sends the targeted player a teleport requests to where you are.");
@@ -35,15 +37,19 @@ public class tbht_main extends JavaPlugin {
             sender.sendMessage("/tpoff - Disables receiving teleport requests.");
             sender.sendMessage("/tpo <Player> - (ADMIN ONLY COMMAND) Instantly teleports you to another player.");
             return false;
-        } else if (label.equalsIgnoreCase("tpa")){
-            if (sender instanceof Player){
-                Player player = (Player)sender;
-                if (args.length>0){
+        } else if (label.equalsIgnoreCase("tpa")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (args.length > 0) {
                     Player target = Bukkit.getPlayer(args[0]);
                     if (target == null) {
                         sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR:" + ChatColor.RED + " Either the player is currently offline or doesn't exist.");
                         return false;
                     } else {
+                        if (tpoff.contains(target)) {
+                            sender.sendMessage(ChatColor.YELLOW + target.getName() + ChatColor.RED + " has disabled to receive teleport requests!");
+                            return false;
+                        }
                         sender.sendMessage(ChatColor.GREEN + "Teleport request send to " + ChatColor.YELLOW + target.getName() + ChatColor.GREEN + "!");
                         sender.sendMessage(ChatColor.GREEN + "They've got " + ChatColor.AQUA + "60 " + ChatColor.GREEN + "seconds to respond!");
                         sender.sendMessage(ChatColor.GREEN + "To cancel the request use " + ChatColor.AQUA + "/tpcancel " + ChatColor.GREEN + "to cancel it.");
@@ -76,6 +82,10 @@ public class tbht_main extends JavaPlugin {
                         sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR:" + ChatColor.RED + " Either the player is currently offline or doesn't exist.");
                         return false;
                     } else {
+                        if (tpoff.contains(target)) {
+                            sender.sendMessage(ChatColor.YELLOW + target.getName() + ChatColor.RED + " has disabled to receive teleport requests!");
+                            return false;
+                        }
                         sender.sendMessage(ChatColor.GREEN + "Teleport request send to " + ChatColor.YELLOW + target.getName() + ChatColor.GREEN + "!");
                         sender.sendMessage(ChatColor.GREEN + "They've got " + ChatColor.AQUA + "60 " + ChatColor.GREEN + "seconds to respond!");
                         sender.sendMessage(ChatColor.GREEN + "To cancel the request use " + ChatColor.AQUA + "/tpcancel " + ChatColor.GREEN + "to cancel it.");
@@ -99,17 +109,29 @@ public class tbht_main extends JavaPlugin {
                     return false;
                 }
             }
-        } else if (label.equalsIgnoreCase("tpayes")){
+        } else if (label.equalsIgnoreCase("tpayes")) {
             if (sender instanceof Player) {
-                Player player = (Player)sender;
-                if (TeleportRequest.getRequest(player) != null){
+                Player player = (Player) sender;
+                if (TeleportRequest.getRequest(player) != null) {
                     TeleportRequest request = TeleportRequest.getRequest(player);
                     request.getRequester().sendMessage(ChatColor.YELLOW + "" + player.getName() + ChatColor.GREEN + " has accepted your teleport request!");
                     player.sendMessage(ChatColor.GREEN + "You've accepted the teleport request!");
                     if (request.getType() == TeleportRequest.TeleportType.TPA_HERE) {
-                        player.teleport(request.getRequester());
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                player.teleport(request.getRequester());
+                            }
+                        }.runTaskLater(this, 60); // 3 seconds
+                        player.sendMessage(ChatColor.GREEN + "Teleporting in " + ChatColor.AQUA + "3 seconds" + ChatColor.GREEN + ", please don't move!"); //TODO Add checker for movement!);
                     } else {
-                        request.getRequester().teleport(player);
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                request.getRequester().teleport(player);
+                            }
+                        }.runTaskLater(this, 60); // 3 seconds
+                        request.getRequester().sendMessage(ChatColor.GREEN + "Teleporting in " + ChatColor.AQUA + "3 seconds" + ChatColor.GREEN + ", please don't move!"); //TODO Add checker for movement!
                     }
                     request.destroy();
                     return false;
@@ -133,12 +155,12 @@ public class tbht_main extends JavaPlugin {
                     return false;
                 }
             }
-        } else if (label.equalsIgnoreCase("tpo")){
+        } else if (label.equalsIgnoreCase("tpo")) {
             if (sender instanceof Player) {
-                Player player = (Player)sender;
-                if (args.length>0){
+                Player player = (Player) sender;
+                if (args.length > 0) {
                     Player target = Bukkit.getPlayer(args[0]);
-                    if (target == null){
+                    if (target == null) {
                         sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR:" + ChatColor.RED + " Either the player is currently offline or doesn't exist.");
                         return false;
                     } else {
@@ -151,9 +173,9 @@ public class tbht_main extends JavaPlugin {
                     return false;
                 }
             }
-        } else if (label.equalsIgnoreCase("tpacancel"))
+        } else if (label.equalsIgnoreCase("tpacancel")) {
             if (sender instanceof Player) {
-                Player player = (Player)sender;
+                Player player = (Player) sender;
                 if (TeleportRequest.getRequestByRequester(player) != null) {
                     TeleportRequest request = TeleportRequest.getRequestByRequester(player);
                     player.sendMessage(ChatColor.GREEN + "You have canceled your teleport request.");
@@ -162,6 +184,97 @@ public class tbht_main extends JavaPlugin {
                     return false;
                 }
             }
+        } else if (label.equalsIgnoreCase("tpaall")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                for (Player target : Bukkit.getOnlinePlayers()) {
+                    if (target != player) {
+                        target.sendMessage(ChatColor.GREEN + "The Player " + ChatColor.YELLOW + sender.getName() + ChatColor.GREEN + " wants to teleport you to them!");
+                        target.sendMessage(ChatColor.GREEN + "If you want to accept it use " + ChatColor.AQUA + "/tpayes " + ChatColor.GREEN + ", if not use" + ChatColor.AQUA + "/tpano" + ChatColor.GREEN + ".");
+                        target.sendMessage(ChatColor.GREEN + "You've got " + ChatColor.AQUA + "60 " + ChatColor.GREEN + "seconds to respond to the request!");
+                        BukkitRunnable run = new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                TeleportRequest.removeRequest(TeleportRequest.getRequest(player));
+                            }
+                        };
+                        run.runTaskLater(this, 1200); // 60 seconds
+                        TeleportRequest request = new TeleportRequest(player, target, run, TeleportRequest.TeleportType.TPA_HERE); // Creates a new teleport request.
+                        TeleportRequest.addRequest(request);
+                    }
+                }
+            }
+        } else if (label.equalsIgnoreCase("tpoff")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (!tpoff.contains(player)) {
+                    tpoff.add(player);
+                    sender.sendMessage(ChatColor.GREEN + "Successfully disabled teleport requests!");
+                    sender.sendMessage(ChatColor.GREEN + "You can no longer receive any teleport requests.");
+                    sender.sendMessage(ChatColor.AQUA + "If you want to receive teleport requests type " + ChatColor.YELLOW + "/tpon " + ChatColor.AQUA + "to enable it.");
+                }
+            }
+        } else if (label.equalsIgnoreCase("tpon")) {
+            if (sender instanceof Player) {
+                Player player = (Player)sender;
+                if (tpoff.contains(player)) {
+                    tpoff.remove(player);
+                    sender.sendMessage(ChatColor.GREEN + "Successfully enabled teleport requests!");
+                    sender.sendMessage(ChatColor.GREEN + "You can now receive teleport requests.");
+                }
+            }
+        } else if (label.equalsIgnoreCase("tpblock")) {
+            if (sender instanceof Player){
+                Player player = (Player)sender;
+                if (args.length>0){
+                    OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+                    if (target == null){
+                        sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR:" + ChatColor.RED + " The player doesn't exist.");
+                        return false;
+                    } else {
+                        if (TeleportBlock.getBlockedPlayers(player).contains(target.getPlayer())){
+                            sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR:" + ChatColor.RED + " The player is already blocked.");
+                            return false;
+                        } else {
+                            try {
+                                TeleportBlock.addBlockedPlayer(player, target.getPlayer());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            sender.sendMessage(ChatColor.YELLOW + target.getName() + ChatColor.GREEN + " has been blocked.");
+                            return false;
+                        }
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR:" + ChatColor.RED + " You must include a player name!");
+                    return false;
+                }
+            }
+        } else if (label.equalsIgnoreCase("tpunblock")) {
+            if (sender instanceof Player){
+                Player player = (Player)sender;
+                if (args.length>0){
+                    OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+                    if (target == null){
+                        sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR:" + ChatColor.RED + " The player doesn't exist.");
+                        return false;
+                    } else {
+                        if (TeleportBlock.getBlockedPlayers(player).contains(target.getPlayer())){
+                            try {
+                                TeleportBlock.remBlockedPlayer(player, target.getPlayer());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            sender.sendMessage(ChatColor.GREEN + "Successfully unblocked " + ChatColor.YELLOW + target.getName() + ChatColor.GREEN + " .");
+                            return false;
+                        }
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR:" + ChatColor.RED + " You must include a player name!");
+                    return false;
+                }
+            }
+        }
         return false;
     }
 }
