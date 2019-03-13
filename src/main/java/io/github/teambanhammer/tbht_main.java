f56c56caa50cf12f1e9969da2b74a3a153d31ace
Niestrat99 <niestrat99@gmail.com>
2019-03-13 21:38:30 +0100

    Facial Stuff

--- src/main/java/io/github/teambanhammer/tbht_main.java
+++ src/main/java/io/github/teambanhammer/tbht_main.java
@@ -89,40 +89,40 @@ private static WorldBorder worldBorder;
                     return false;
                 } else if (args[0].equalsIgnoreCase("teleport")){
                     sender.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "Teleport help");
-                    sender.sendMessage(ChatColor.GOLD + "/tpa <player> - Sends a request to teleport to the player.");
-                    sender.sendMessage(ChatColor.GOLD + "/tpahere <player> - Sends a request to the player to teleport to you");
-                    sender.sendMessage(ChatColor.GOLD + "/tpaccept - Accepts a player's teleport request.");
-                    sender.sendMessage(ChatColor.GOLD + "/tpdeny - Declines a player's teleport request.");
-                    sender.sendMessage(ChatColor.GOLD + "/tpcancel - Lets you cancel the request you have sent to a player.");
-                    sender.sendMessage(ChatColor.GOLD + "/tpon - Enables teleport requests to you.");
-                    sender.sendMessage(ChatColor.GOLD + "/tpoff - Disables teleport requests to you.");
-                    sender.sendMessage(ChatColor.GOLD + "/tpblock <player> - Blocks the player so that they cannot send you teleport requests anymore.");
-                    sender.sendMessage(ChatColor.GOLD + "/tpunblock <player> - Unblocks the player so that they can send you teleport requests.");
+                    sender.sendMessage(ChatColor.GOLD + "- /tpa <player> - Sends a request to teleport to the player.");
+                    sender.sendMessage(ChatColor.GOLD + "- /tpahere <player> - Sends a request to the player to teleport to you");
+                    sender.sendMessage(ChatColor.GOLD + "- /tpaccept - Accepts a player's teleport request.");
+                    sender.sendMessage(ChatColor.GOLD + "- /tpdeny - Declines a player's teleport request.");
+                    sender.sendMessage(ChatColor.GOLD + "- /tpcancel - Lets you cancel the request you have sent to a player.");
+                    sender.sendMessage(ChatColor.GOLD + "- /tpon - Enables teleport requests to you.");
+                    sender.sendMessage(ChatColor.GOLD + "- /tpoff - Disables teleport requests to you.");
+                    sender.sendMessage(ChatColor.GOLD + "- /tpblock <player> - Blocks the player so that they cannot send you teleport requests anymore.");
+                    sender.sendMessage(ChatColor.GOLD + "- /tpunblock <player> - Unblocks the player so that they can send you teleport requests.");
                     if (sender.hasPermission("tbh.tp.admin.help")){
-                        sender.sendMessage(ChatColor.GOLD + "/tpo <player> - Instantly teleports you to the player.");
-                        sender.sendMessage(ChatColor.GOLD + "/tpohere <player> - Instantly teleports the player to you.");
-                        sender.sendMessage(ChatColor.GOLD + "/tpall - Sends a teleport request to every online player to teleport to you.");
+                        sender.sendMessage(ChatColor.GOLD + "- /tpo <player> - Instantly teleports you to the player.");
+                        sender.sendMessage(ChatColor.GOLD + "- /tpohere <player> - Instantly teleports the player to you.");
+                        sender.sendMessage(ChatColor.GOLD + "- /tpall - Sends a teleport request to every online player to teleport to you.");
                         return false;
                     }
                 } else if (args[0].equalsIgnoreCase("warps")) {
                     sender.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "Warps help");
-                    sender.sendMessage(ChatColor.GOLD + "/warp <warp name> - Teleports you to an existing warp point.");
-                    sender.sendMessage(ChatColor.GOLD + "/warps - Gives you a list of warps.");
+                    sender.sendMessage(ChatColor.GOLD + "- /warp <warp name> - Teleports you to an existing warp point.");
+                    sender.sendMessage(ChatColor.GOLD + "- /warps - Gives you a list of warps.");
                     if (sender.hasPermission("tbh.tp.admin.help")) {
-                        sender.sendMessage(ChatColor.GOLD + "/warp set <warp name> - Sets a warp point at your location.");
-                        sender.sendMessage(ChatColor.GOLD + "/warp delete <warp name> - Deletes an existing warp point.");
+                        sender.sendMessage(ChatColor.GOLD + "- /warp set <warp name> - Sets a warp point at your location.");
+                        sender.sendMessage(ChatColor.GOLD + "- /warp delete <warp name> - Deletes an existing warp point.");
                         return false;
                     }
                 } else if (args[0].equalsIgnoreCase("Spawn")) {
                     sender.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "Spawn help");
-                    sender.sendMessage(ChatColor.GOLD + "/spawn - Teleports you to the spawn point.");
+                    sender.sendMessage(ChatColor.GOLD + "- /spawn - Teleports you to the spawn point.");
                     if (sender.hasPermission("tbh.tp.admin.help")) {
-                        sender.sendMessage(ChatColor.GOLD + "/setspawn - Sets a spawn point at your location.");
+                        sender.sendMessage(ChatColor.GOLD + "- /setspawn - Sets a spawn point at your location.");
                         return false;
                     }
                 } else if (args[0].equalsIgnoreCase("RandomTP")) {
                         sender.sendMessage(ChatColor.AQUA + "" + ChatColor.BOLD + "RandomTP help");
-                        sender.sendMessage(ChatColor.GOLD + "/rtp - Teleports you to a random location.");
+                        sender.sendMessage(ChatColor.GOLD + "- /rtp - Teleports you to a random location.");
                 } else {
                     sender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "ERROR: " + ChatColor.RED + "This category does not exist! Type /athelp to get a list of existing categories.");
                     return false;
